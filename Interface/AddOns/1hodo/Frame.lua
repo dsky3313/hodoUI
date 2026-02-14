a7ec6fca19556a77cd2e0b24a7976c7682dc4b66
dsky3313 <dsky3313@gmail.com>
2026-02-14 09:45:58 +0900

    update pre-midnight

--- Interface/AddOns/1hodo/Frame.lua
+++
@@ -1,5 +0,0 @@
-TalkingHeadFrame:SetScale(0.8);
-
-MainMenuBarBackpackButton:SetScale(0.7)
-
-GameMenuFrame:SetScale(0.9)
--- Interface/AddOns/BlizzHUDTweaks/API.lua
+++
@@ -1,9 +0,0 @@
-local addon = LibStub("AceAddon-3.0"):GetAddon("BlizzHUDTweaks")
-
-local API = {
-  LoadProfile = function(name)
-    addon:LoadProfileByName(name)
-  end
-}
-
-_G["BlizzHUDTweaks"] = API
